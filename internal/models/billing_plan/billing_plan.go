e644d9565a4387a478d4de7857c7b77a5267eab9
griffnb <griffnb@gmail.com>
2025-11-16 15:49:27 -0700

    format
    Signed-off-by: griffnb <griffnb@gmail.com>

--- internal/models/billing_plan/billing_plan.go
+++ internal/models/billing_plan/billing_plan.go
@@ -26,14 +26,14 @@ type Structure struct {
 
 type DBColumns struct {
 	base.Structure
-	Name         *fields.StringField                    `column:"name"          type:"text"     default:""`
-	InternalName *fields.StringField                    `column:"internal_name" type:"text"     default:""`
-	BillingCycle *fields.IntConstantField[BillingCycle] `column:"billing_cycle" type:"smallint" default:"0"`
-	Price        *fields.DecimalField                   `column:"price"         type:"numeric"  default:"0"  public:"view" scale:"4" precision:"10"`
-	FeatureSet   *fields.StructField[*FeatureSet]       `column:"feature_set"   type:"jsonb"    default:"{}" public:"view"`
-	Properties   *fields.StructField[*Properties]       `column:"properties"    type:"jsonb"    default:"{}" public:"view"`
-	Level        *fields.IntField                       `column:"level"         type:"smallint" default:"0"  public:"view"`
-	IsDefault    *fields.IntField                       `column:"is_default"    type:"smallint" default:"0"                                         index:"true"`
+	Name         *fields.StringField                    `public:"view" column:"name"          type:"text"     default:""`
+	InternalName *fields.StringField                    `              column:"internal_name" type:"text"     default:""`
+	BillingCycle *fields.IntConstantField[BillingCycle] `public:"view" column:"billing_cycle" type:"smallint" default:"0"`
+	Price        *fields.DecimalField                   `public:"view" column:"price"         type:"numeric"  default:"0"  scale:"4" precision:"10"`
+	FeatureSet   *fields.StructField[*FeatureSet]       `public:"view" column:"feature_set"   type:"jsonb"    default:"{}"`
+	Properties   *fields.StructField[*Properties]       `public:"view" column:"properties"    type:"jsonb"    default:"{}"`
+	Level        *fields.IntField                       `public:"view" column:"level"         type:"smallint" default:"0"`
+	IsDefault    *fields.IntField                       `public:"view" column:"is_default"    type:"smallint" default:"0"                           index:"true"`
 }
 
 type JoinData struct {
