c53d7f98b52950d7bd6dc174955e14713785786a
griffnb <griffnb@gmail.com>
2026-01-31 14:20:05 -0700

    remove all this
    Signed-off-by: griffnb <griffnb@gmail.com>

--- internal/models/lifecycle_test_placeholder/functions.go
+++
@@ -1,3 +0,0 @@
-package lifecycle_test_placeholder
-
-// This file contains additional helper functions for the LifecycleTestPlaceholder model
--- internal/models/lifecycle_test_placeholder/response.go
+++
@@ -1,8 +0,0 @@
-package lifecycle_test_placeholder
-
-import "github.com/griffnb/core/lib/sanitize"
-
-// ToPublicJSON converts the model to a sanitized JSON representation for public consumption
-func (this *LifecycleTestPlaceholder) ToPublicJSON() any {
-	return sanitize.SanitizeModel(this, &Structure{})
-}
